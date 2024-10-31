6ad130e7d0700884fe05ca184e832204970f7277
Scott Soucit <35660946+soucit47@users.noreply.github.com>
2024-10-31 16:17:49 +0000

    added random number function

--- mixedMessage.js
+++ mixedMessage.js
@@ -1,1 +1,19 @@
 //Mixed message file that will return a random Star Trek quote to the screen when run
+
+// Arrays contain the part quotes all must have same number of cells
+const quotesOne = ['Live Long', 'i am a', 'I have been','A lie is'];
+const quotesTwo = ['and', 'doctor','and always will be','a very poor way'];
+const quoatesThree = ['prosper','not a bricklayer','your friend','to say hello'];
+
+
+//random number function
+function randomNumber() {
+    return Math.floor(Math.random() * (quotesOne.length));
+};
+
+//sentance builder function
+
+
+
+//call to display on screen
+console.log(randomNumber());
